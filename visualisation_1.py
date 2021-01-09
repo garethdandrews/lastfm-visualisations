8b73c148d558cc00843260aa8eb961d8996f50fb
gda <garethdandrews@gmail.com>
2021-01-09 00:23:38 +0000

    get the plays for the top artist for every week since the user registered

--- visualisation_1.py
+++ visualisation_1.py
@@ -6,40 +6,23 @@ import pandas as pd
 top_artists = api_service.user_get_top_artists('overall')
 top_artist = top_artists['artist'][0]['name']
 
-print("Top Artist: " + top_artist)
-
-
-# get user info
-info = api_service.user_get_info()
-registered = info['registered']['unixtime']
-print("Registered(datetime): " + str(unix_to_datetime(registered)))
-
+df = pd.DataFrame(columns=['week_from',top_artist])
 
 # find the week the user registered
+info = api_service.user_get_info()
+registered = int(info['registered']['unixtime'])
 weekly_chart_list = api_service.user_get_weekly_chart_list()
 
+i = 0
 user_registered = False
-registered = int(registered)
 for n, week in enumerate(weekly_chart_list['chart']):
     if registered  >= int(week['from']) and registered <= int(week['to']):
         user_registered = True
         
     if user_registered:
-        # look at weekly artist charts
-        print("hello")
-
-
-registered_week = weekly_chart_list['chart'][629]
-
-print(registered_week)
-print(str(unix_to_datetime(registered_week['from'])))
-
-weekly_artist_chart = api_service.user_get_weekly_artist_chart(registered_week)
-print(weekly_artist_chart)
-
-df = pd.DataFrame(columns=['week_from',top_artist])
-top_artist_plays = next(artist['playcount'] for artist in weekly_artist_chart['artist'] if artist['name'] == top_artist)
-print(top_artist_plays)
-# df.loc[0] = [str(unix_to_datetime(week['from'])), 1000]
+        weekly_artist_chart = api_service.user_get_weekly_artist_chart(week)
+        top_artist_plays = next((artist['playcount'] for artist in weekly_artist_chart['artist'] if artist['name'] == top_artist), 0)
+        df.loc[i] = [str(unix_to_datetime(week['from'])), top_artist_plays]
+        i += 1
 
 print(df.head())
